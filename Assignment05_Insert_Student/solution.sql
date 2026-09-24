da6e1e13c1ccf9d705e1dcaf6294dc3e4520e54b
dineshagali6666-beep <dineshagali6666@gmail.com>
2026-09-24 12:54:28 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -11,13 +11,11 @@ CREATE TABLE Student(
     Email VARCHAR(30),
     PhoneNumber BIGINT
 );
-
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+create database college;
+use college;
+create table Student( studentID int(5) primary key,studentName 
+varchar(20) not null, gender varchar(10) not null,departmentID 
+int(5));
+insert into Student values(1001,"Arun","Male",101),
+(1002,"Divya","Female",102),(1003,"Karthik","Male",101);
+select * from Student;
